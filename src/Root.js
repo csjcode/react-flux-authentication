eff00715e333123c469f282fe54cfb86527ac69f
csjcode <christophersstjohn@gmail.com>
2016-05-19 13:58:56 +0700

    Commented out & moved Contact component

--- src/Root.js
+++ src/Root.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Router, Route, IndexRoute } from 'react-router';
 import Index from './components/Index';
 // import ContactDetail from './components/ContactDetail';
+  // <Route path='/contact/:id' component={ContactDetail} />
 
 import App from './components/App';
 
@@ -15,7 +16,7 @@ class Root extends Component {
       <Router history={this.props.history}>
         <Route path='/' component={App}>
           <IndexRoute component={Index}/>
-          // <Route path='/contact/:id' component={ContactDetail} />
+
         </Route>
       </Router>
     );
